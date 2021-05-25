5056baa2a018cc8f5dca9cd8aec4d67b7b663495
George Goodall <george.goodall@hotmail.co.uk>
2021-05-25 16:54:12 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,40 @@ import Tours from './Tours'
 
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);  
+  const fetchTours = async () => {
+    // when we are fetching the data, we want loading to be true
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    }catch(error) {
+      setLoading(false);
+      console.log(error);
+    }
+    
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return ( 
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 }
 
 export default App
